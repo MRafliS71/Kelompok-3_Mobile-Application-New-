656d6c1ddc0263ad72657f9525608294666f5d29
raymosaid <raymosaid@users.noreply.github.com>
2022-11-28 00:19:16 +0700

    Backend nih BOSSS!!!!

--- KerabatApplication/app/src/main/java/com/kelompok3/fallhuge/SignInKerabatActivity.java
+++ KerabatApplication/app/src/main/java/com/kelompok3/fallhuge/SignInKerabatActivity.java
@@ -2,19 +2,29 @@ package com.kelompok3.fallhuge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.net.HttpURLConnection;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignInKerabatActivity extends AppCompatActivity {
 
+    TextInputEditText editTextUsername, editTextFirstname, editTextLastname, editTextEmail, editTextPassword;
     private Button buatakunButton;
     private TextView masukDisini;
 
-
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +34,63 @@ public class SignInKerabatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in_kerabat);
         getSupportActionBar().hide();
 
+        editTextUsername = findViewById(R.id.editTextTextUsername);
+        editTextFirstname = findViewById(R.id.editTextTextFirstname);
+        editTextLastname = findViewById(R.id.editTextTextLastname);
+        editTextEmail = findViewById(R.id.editTextTextEmail);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
+        //Set tombol
         buatakunButton = findViewById(R.id.buatakunButton);
         buatakunButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(SignInKerabatActivity.this, MainpageKerabatActivity.class);
-                SignInKerabatActivity.this.startActivity(myIntent);
+                String username, firstname, lastname, email, password;
+                username = String.valueOf(editTextUsername.getText());
+                firstname = String.valueOf(editTextFirstname.getText());
+                lastname = String.valueOf(editTextLastname.getText());
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(!username.equals("") && !firstname.equals("") && !lastname.equals("") && !email.equals("") && !password.equals("")) {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[5];
+                            field[0] = "username";
+                            field[1] = "firstname";
+                            field[2] = "lastname";
+                            field[3] = "email";
+                            field[4] = "password";
+                            //Creating array for data
+                            String[] data = new String[5];
+                            data[0] = username;
+                            data[1] = firstname;
+                            data[2] = lastname;
+                            data[3] = email;
+                            data[4] = password;
+                            PutData putData = new PutData("http://192.168.119.70/mobappproject/akunKerabat/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+
+                                    if(result.equals("Sign Up Success")){
+                                        //Set agar tombol bisa diklik dan pindah page
+                                        Intent myIntent = new Intent(SignInKerabatActivity.this, MainpageKerabatActivity.class);
+                                        SignInKerabatActivity.this.startActivity(myIntent);
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(), "Harap isis seluruh kolom yang tersedia", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
