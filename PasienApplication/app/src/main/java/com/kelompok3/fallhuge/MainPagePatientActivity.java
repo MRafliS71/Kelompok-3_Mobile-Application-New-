a5e2d251c2db191544d07a6a63db56c664c714ff
MRafliS71 <mraflis@upi.edu>
2022-12-13 10:12:12 +0700

    Deteksi sensor
    deteksi sensor berdasarkan percepatan.

--- PasienApplication/app/src/main/java/com/kelompok3/fallhuge/MainPagePatientActivity.java
+++ PasienApplication/app/src/main/java/com/kelompok3/fallhuge/MainPagePatientActivity.java
@@ -1,24 +1,35 @@
 package com.kelompok3.fallhuge;
 
+import static java.lang.Math.sqrt;
+
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainPagePatientActivity extends AppCompatActivity {
+public class MainPagePatientActivity extends AppCompatActivity implements SensorEventListener {
 
     private ImageButton pesanButton, kerabatButton, accountButton, bantuanButton, notifikasiButton;
     private Button buttonFallSimulation;
     AlertDialog dialog;
+
+    //======== Variable Sensor Section ==========
+    private SensorManager sm;
+    private Sensor senAccel;
+    private TextView tvHasil;
+    private boolean isJatuh;
 
     @Override
     public void onBackPressed() {
@@ -103,5 +114,71 @@ public class MainPagePatientActivity extends AppCompatActivity {
                 MainPagePatientActivity.this.startActivity(myIntent);
             }
         });
+
+        // =========TES SENCOR SECTION===========
+        String TAG = "sensor_tes";
+
+        //cek apakah sensor tersedia
+        sm = (SensorManager)    getSystemService(getApplicationContext().SENSOR_SERVICE);
+        senAccel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (senAccel != null){
+            Log.d(TAG,"Sukses, device punya sensor accelerometer!");
+        }
+        else {
+            // gagal, tidak ada sensor accelerometer.
+            Log.d(TAG,"Tidak ada sensor accelerometer!");
+        }
+
+        tvHasil = findViewById(R.id.tvHasil);
     }
+
+    // Fungsi perubahan pergerakan sensor
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double ax=0,ay=0,az=0;
+        double ac=0;
+        // menangkap perubahan nilai sensor
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
+            ax=sensorEvent.values[0];
+            ay=sensorEvent.values[1];
+            az=sensorEvent.values[2];
+        }
+        //Rumus akselerasi
+        ac = sqrt(az*az+ax*ax+ay*ay);
+        //jika akselerasi melebihi 20 maka terdeteksi jatuh
+        if  (ac>=20) {
+            isJatuh = true;
+        }
+        //jika jatuh bernilai true maka akan menampilkan page pop up
+        if (isJatuh) {
+            Intent myIntent = new Intent(MainPagePatientActivity.this, PopUpFallDetection.class);
+            MainPagePatientActivity.this.startActivity(myIntent);
+        } else{
+            long timestamp = System.currentTimeMillis();
+            // Menampilkan log dari accelerometer beserta timestamp
+            String msg = "X: " + ax + ", Y: " + ay + ", Z: " + az + ", Timestamp: " + timestamp + ", Acceleration: " + ac;
+            Log.d("sensor_tes", msg);
+            //tvHasil.setText(msg);
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this, senAccel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
